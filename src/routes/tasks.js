f8595e82f306bb2adb49c0ade7095c7f31a3bb93
Goku144 <ytyui31@gmail.com>
2026-04-07 11:04:19 +0100

    Refactor Routes

---
+++ src/routes/tasks.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+let tasks = [
+  { id: 1, title: "Initial task", completed: true },
+  { id: 2, title: "Install Git and Node.js", "completed": true },
+  { id: 3, title: "Learn DevOps basics", completed: false }
+  
+];
+
+router.get('/', (req, res) => {
+  res.json(tasks);
+});
+
+router.post('/', (req, res) => {
+  const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+module.exports = router;
